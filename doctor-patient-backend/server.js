7ddd2b8895174e3661b44843ecd5feaefb60c1e5
aryanbarde80 <aryanbarde80@gmail.com>
2025-07-24 13:03:10 +0530

    added aditional port

--- doctor-patient-backend/server.js
+++ doctor-patient-backend/server.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 import "dotenv/config";
 import './cron.js';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000 || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server ready on port ${PORT}`);
